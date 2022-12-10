9506e6d8c93aebc34196840e08f11f2bf308420f
Metroynome <yort124@gmail.com>
2022-12-10 17:33:54 -0500

    Updates to kernel.c and code.c/linkfile

--- Kernel/main.c
+++ Kernel/main.c
@@ -1,20 +1,11 @@
 #include <tamtypes.h>
 #include <kernel.h>
 #include <libdl/dl.h>
-#include <libdl/player.h>
 #include <libdl/pad.h>
 #include <libdl/game.h>
 #include <libdl/string.h>
-#include <libdl/stdio.h>
-#include <libdl/gamesettings.h>
 #include <libdl/graphics.h>
 #include <libdl/ui.h>
-#include <libdl/math.h>
-#include <libdl/math3d.h>
-#include <libdl/hud.h>
-#include <libdl/music.h>
-#include <libdl/weapon.h>
-#include <libdl/team.h>
 
 #define EngineAddr 0x80047010
 
@@ -26,7 +17,7 @@ u32 Engine[] = {
 	0x27BDFFE0,
 	0x7FBF0000,
 	0x7FA20010,
-	// 0x0C011C12, // jal to jump to codes?
+	// 0x0C014000, // jal to jump to codes
 	0x00000000,
 	0x3C028000,
 	0x34420234,
@@ -35,7 +26,7 @@ u32 Engine[] = {
 	0x7BA20010,
 	0x03400008,
 	0x27BD0020,
-	
+
 };
 
 void HookKernel(void)
@@ -43,7 +34,9 @@ void HookKernel(void)
 	DI();
 	ee_kmode_enter();
 		memcpy((u32*)(int)EngineAddr, Engine, sizeof(Engine));						/* Install the NetCheat Engine into the kernel */
-		// memset((u32*)CodesAddr, 0 , code_len + 8);									/* Clear code area */
+		// memset((u8*)CodesAddr, 0, 0x61440);	
+		memcpy((u32*)(CodesAddr + 8), (u32*)0x01E00000, 0xffff);
+		// memcpy((u32*)0x000F0000, (u32*)CodesAddr, 61440);								/* Clear code area */
 		// memcpy((u32*)(CodesAddr + 8), (u32*)0x000F0000, code_len);					/* Install the codes into the kernel */
 		*(u32*)((int)EngineAddr - 0x18) = (CodesAddr + 0x10); 							/* Pointer to initial code */
 		*(u32*)((int)EngineAddr - 0x10) = (CodesAddr + 0x10); 							/* Pointer to current code */
@@ -66,8 +59,7 @@ int main(void)
 	// Call this first
 	dlPreUpdate();
 
-	if (*(u32*)HookAddr != HookValue)
-		HookKernel();
+	HookKernel();
 
 	// Call this last
 	dlPostUpdate();
